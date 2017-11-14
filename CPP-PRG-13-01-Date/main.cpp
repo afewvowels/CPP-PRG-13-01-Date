3473710b2eeb911ee9541914fbc2e24acbe49aa9
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-13 19:02:19 -0500

    Prompt written.

--- CPP-PRG-13-01-Date/main.cpp
+++ CPP-PRG-13-01-Date/main.cpp
@@ -5,11 +5,21 @@
 //  Created by Keith Smith on 11/13/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Design a class called Date. The class should store a date in three integers: month, day,
+//  and year. There should be member functions to print the date in the following forms:
+//
+//      12/25/2018
+//      December 25, 2018
+//      25 December 2018
+//
+//  Demonstrate the class by writing a complete program implementing it.
+//
+//  Input Validation: Do not accept values for the day greater than 31 or less than 1. Do
+//  not accept values for the month greater than 12 or less than 1.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
